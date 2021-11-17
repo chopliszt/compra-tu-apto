911870c2c6f938a41ac09af1c6936fcf738c0f9b
Juan Camilo Infante <juan.infante@perspektiva360.com>
2021-11-17 11:45:30 -0500

    Se instaló la librería npm de material ui icons para tener soporte de iconos. En el CartWidget, se cambió el icono, antes se usaba el de Semantic-ui y ahora se usa el de material-ui. Se colocó la funcionalidad del badge de material-ui, para que en el icono de carrito uno vea cuantos items hay allí para comprar

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { Badge, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { Icon } from "semantic-ui-react";
 
 const CartWidget = () => {
   return (
     <div>
-      <Icon name="in cart" size="big" />
+      {/* <Icon name="in cart" size="big" /> */}
+      <IconButton aria-label="cart">
+        <Badge badgeContent={4} color="secondary">
+          <ShoppingCartIcon fontSize="medium" />
+          {/* <Icon name="in cart" size="big" /> */}
+        </Badge>
+      </IconButton>
     </div>
   );
 };
