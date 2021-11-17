cc555131fcd9ddc4a8e0bc7728b2cffd6b9d913a
Juan Camilo Infante <juan.infante@perspektiva360.com>
2021-11-17 10:50:59 -0500

    se instaló Material UI vía NPM, después se trajo el componente card de allí y se colocó en el componente ItemCount. Para centrarlo se usó el container y clase row de bootstrap

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const ItemCount = () => {
+export default function ImgMediaCard() {
   return (
-    <div>
-      <h1>Hola mundo desde ItemCount</h1>
+    <div className="container">
+      <div className="row mb-5 justify-content-center">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Apartamento 1
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Este apto Lizards are a widespread group of squamate reptiles,
+              with over 6,000 species, ranging across all continents except
+              Antarctica
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      </div>
     </div>
   );
-};
-
-export default ItemCount;
+}
